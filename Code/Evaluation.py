458fa39b9b708a35ae207a06ceeb10f6068b876e
Najib Hasan <86787897+MdNajibHasan@users.noreply.github.com>
2025-11-17 18:24:44 -0600

    Add files via upload

--- Code/Evaluation.py
+++ Code/Evaluation.py
@@ -5,76 +5,13 @@ import numpy as np
 metric_list=[]
 global a
 a=0
-#update location
-# Dataset='/home/szs0239/Feature_Mining/Dataset/'
-# Results_Directory='/home/szs0239/Feature_Mining/Results/Privacy Policy/Topic Embedding/'
-#must be the locations where you saved EM word and sentence files
+
 Directory="/Users/souvika/Library/CloudStorage/OneDrive-AuburnUniversity/Spring 2023/BanglaNLP/venv/Results/LASER/A3P2/"
-# Results_Directory_Word='/home/szs0239/NewsConceptMining/Sentence Embedding/P2A2/'
-# Results_Directory_Sentence='/home/szs0239/Feature_Mining/Results/Privacy Policy/Topic Embedding/Sentence/'
 Dataset='/Users/souvika/Library/CloudStorage/OneDrive-AuburnUniversity/Spring 2023/BanglaNLP/venv/dataset/'
-#Results_Directory='/Users/souvikasarkar/Documents/OneDrive - Auburn University/FeatureMining/Results/Covid/BERT/'
-#Results_Directory_Word='/Users/souvikasarkar/Documents/OneDrive - Auburn University/FeatureMining/Results/News/Temp/Word/'
+
 
 
 def Count_TP_FP_FN():
-    # theta1=[]
-    # precision1=[]
-    # recall1=[]
-    # f_measure1=[]
-    # theta2=[]
-    # precision2=[]
-    # recall2=[]
-    # f_measure2=[]
-    #
-    # with open(Results_Directory+"Evaluation_Medical_Hybrid_Sentence.json", "r") as f:
-    #     key = json.loads(f.read())
-    # with open(Results_Directory+"Evaluation_Medical_Hybrid_Word.json", "r") as f:
-    #     key1 = json.loads(f.read())
-    #
-    # for j in range(len(key)):
-    #     print(j)
-    #     theta1.append(key[j]['Color Probability'])
-    #     precision1.append(key[j]['Precision'])
-    #     recall1.append(key[j]['Recall'])
-    #     f_measure1.append(key[j]['F-Measure'])
-    #     theta2.append(key1[j]['Color Probability'])
-    #     precision2.append(key1[j]['Precision'])
-    #     recall2.append(key1[j]['Recall'])
-    #     f_measure2.append(key1[j]['F-Measure'])
-    #
-    # # ax = plt.subplots()
-    #
-    # Presicion = plt.figure()
-    # plt.ylim(0, 1)
-    # plt.xlim(0, 1)
-    # plt.plot(theta1, precision1,label='EM_Word')
-    # plt.plot(theta2, precision2,label='EM_Sentence')
-    # plt.xlabel('Theta')
-    # plt.ylabel('Precision')
-    # plt.legend()
-    # plt.show()
-    # Presicion.savefig(Results_Directory+"Presicion.pdf", bbox_inches='tight')
-    # Recall = plt.figure()
-    # plt.ylim(0, 1)
-    # plt.xlim(0, 1)
-    # plt.plot(theta1, recall1,label='EM_Word')
-    # plt.plot(theta2, recall2,label='EM_Sentence')
-    # plt.xlabel('Theta')
-    # plt.ylabel('Recall')
-    # plt.legend()
-    # plt.show()
-    # Recall.savefig(Results_Directory+"Recall.pdf", bbox_inches='tight')
-    # F_measure = plt.figure()
-    # plt.ylim(0, 1)
-    # plt.xlim(0, 1)
-    # plt.plot(theta1, f_measure1,label='EM_Word')
-    # plt.plot(theta2, f_measure2,label='EM_Sentence')
-    # plt.xlabel('Theta')
-    # plt.ylabel('F1 Measure')
-    # plt.legend()
-    # plt.show()
-    # F_measure.savefig(Results_Directory+"F1 Measure.pdf", bbox_inches='tight')
     theta1 = []
     precision1 = []
     recall1 = []
@@ -104,18 +41,6 @@ def Count_TP_FP_FN():
         key1 = json.loads(f.read())
     with open(Directory+"Evaluation_Covid_EM_Word.json", "r") as f:
         key2 = json.loads(f.read())
-    # with open(Directory+"Evaluation_News_EMR_Sentence_7.json", "r") as f:
-    #     key3 = json.loads(f.read())
-    # with open(Directory+"Evaluation_PrivacyPolicy_TR_Word.json", "r") as f:
-    #     key4 = json.loads(f.read())
-    # with open(Directory+"Evaluation_PrivacyPolicy_Hybrid_Sentence.json", "r") as f:
-    #     key5 = json.loads(f.read())
-    # with open(Directory+"Evaluation_PrivacyPolicy_Hybrid_Word.json", "r") as f:
-    #     key6 = json.loads(f.read())
-    # with open("Covid_Euclidean_Evaluation_Sentence_hybrid.json", "r") as f:
-    #     key5 = json.loads(f.read())
-    # with open("Covid_Cosine_Evaluation_Sentence_hybrid.json", "r") as f:
-    #     key6 = json.loads(f.read())
 
     for j in range(len(key1)):
         print(j)
@@ -127,34 +52,13 @@ def Count_TP_FP_FN():
         precision2.append(key2[j]['Precision'])
         recall2.append(key2[j]['Recall'])
         f_measure2.append(key2[j]['F-Measure'])
-        # theta3.append(key3[j]['Color Probability'])
-        # precision3.append(key3[j]['Precision'])
-        # recall3.append(key3[j]['Recall'])
-        # f_measure3.append(key3[j]['F-Measure'])
-        # theta4.append(key4[j]['Color Probability'])
-        # precision4.append(key4[j]['Precision'])
-        # recall4.append(key4[j]['Recall'])
-        # f_measure4.append(key4[j]['F-Measure'])
-        # theta5.append(key5[j]['Color Probability'])
-        # precision5.append(key5[j]['Precision'])
-        # recall5.append(key5[j]['Recall'])
-        # f_measure5.append(key5[j]['F-Measure'])
-        # theta6.append(key6[j]['Color Probability'])
-        # precision6.append(key6[j]['Precision'])
-        # recall6.append(key6[j]['Recall'])
-        # f_measure6.append(key6[j]['F-Measure'])
-
-    # ax = plt.subplots()
+        
 
     Presicion = plt.figure()
     plt.ylim(0, 1)
     plt.xlim(0, 1)
     plt.plot(theta1, precision1, label='EM_Sentence')
     plt.plot(theta2, precision2, label='EM_Word')
-    # plt.plot(theta3, precision3, label='EM-Sentence_7Concept')
-    # plt.plot(theta3, precision4, label='TR_Word')
-    # plt.plot(theta5, precision5,label='Hybrid_Sentence - Cosine')
-    # plt.plot(theta6, precision6,label='Hybrid_Word - Cosine')
     plt.xlabel('Theta')
     plt.ylabel('Precision')
     plt.legend()
@@ -165,10 +69,6 @@ def Count_TP_FP_FN():
     plt.xlim(0, 1)
     plt.plot(theta1, recall1, label='EM_Sentence')
     plt.plot(theta2, recall2, label='EM_Word')
-    # plt.plot(theta3, recall3, label='EM_Sentence_7Concept')
-    # plt.plot(theta3, recall4, label='TR_Word')
-    # plt.plot(theta5, recall5,label='Hybrid_Sentence - Cosine')
-    # plt.plot(theta6, recall6,label='HHybrid_Word - Cosine')
     plt.xlabel('Theta')
     plt.ylabel('Recall')
     plt.legend()
@@ -179,10 +79,6 @@ def Count_TP_FP_FN():
     plt.xlim(0, 1)
     plt.plot(theta1, f_measure1, label='EM_Sentence')
     plt.plot(theta2, f_measure2, label='EM_Word')
-    # plt.plot(theta3, f_measure3, label='EM_Sentence_7Concept')
-    # plt.plot(theta3, f_measure4, label='TR_Word')
-    # plt.plot(theta5, f_measure5,label='Hybrid_Sentence - Cosine')
-    # plt.plot(theta6, f_measure6,label='Hybrid_Word - Cosine')
     plt.xlabel('Theta')
     plt.ylabel('F1 Measure')
     plt.legend()
@@ -204,9 +100,6 @@ def Evaluate_Inferred_Concepts():
         TP = 0.0
         FP = 0.0
         FN = 0.0
-        # precision = 0
-        # recall = 0
-        # F_Measure = 0
         for j in range(len(data)):
             print("Article Number::", j, "Threshold::", i)
             with open(Directory+filename, "r") as f:
@@ -242,10 +135,7 @@ def Evaluate_Inferred_Concepts():
         metric_dict['Precision'] = precision
         metric_dict['Recall'] = recall
         metric_dict['F-Measure'] = F_Measure
-        metric_list.append(metric_dict)
-        # print(metric_list[a]['Color Probability'])
-        # a +=
-  # Update json file name here to save the result      
+        metric_list.append(metric_dict)     
   with open(Directory+"Bengali_Laser_Zero-shot_A3P2_Evaluation.json", 'w+') as f:
        print("writing json")
        json.dump(metric_list, f, indent=4, separators=(',', ':'))
